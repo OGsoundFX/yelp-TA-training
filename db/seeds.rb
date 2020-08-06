404c714e1fb48a014b7ce09f36359c90890e7b22
Olivier Girardot <olive_girardot@hotmail.com>
2020-08-06 12:15:17 +0200

    added index show actions and views and added a new top restaurant route

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+puts "deleting all restaurants and reviews"
+
+Review.delete_all
+Restaurant.delete_all
+
+puts Restaurant.count
+puts Review.count
+
+puts "creating new restaurants"
 
 20.times do
-  Restaurant.new()
+  name = Faker::Restaurant.name
+  address = Faker::Address.community
+  phone = Faker::PhoneNumber.cell_phone
+  category = ["chinese", "italian", "japanese", "french", "belgian"].sample
+  Restaurant.create(name: name, address: address, phone_number: phone, category: category)
+end
+
+list_ids = Restaurant.all
+array_of_ids = []
+list_ids.each do |restaurant|
+  array_of_ids << restaurant.id
+end
+
+
+100.times do
+  Review.create(content: Faker::Restaurant.review, rating: rand(1..5), restaurant_id: array_of_ids.sample)
+end
+
+puts Restaurant.count
+puts Review.count
+
+
+random = array_of_ids.sample
+puts Restaurant.find(random).name
+puts Restaurant.find(random).address
+puts Restaurant.find(random).category
+reviews = Restaurant.find(random).reviews
+number = 1
+reviews.each do |review|
+  puts "review number #{number}"
+  puts review.content
+  puts review.rating
+  number += 1
 end
