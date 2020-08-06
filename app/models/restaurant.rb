8b1dfdf727c02c746612b91a0f3ee3741e60debb
Olivier Girardot <olive_girardot@hotmail.com>
2020-08-06 10:41:18 +0200

    created models and controllers, routes and added the faker gem

---
+++ app/models/restaurant.rb
@@ -0,0 +1,6 @@
+class Restaurant < ApplicationRecord
+  restaurant_types = ["chinese", "italian", "japanese", "french", "belgian"]
+  has_many :reviews, dependent: :destroy
+  validates :name, :address, :category, :phone_number, presence: true
+  validates :category, inclusion: { in: restaurant_types, message: "%{value} is not a valid size" }
+end
